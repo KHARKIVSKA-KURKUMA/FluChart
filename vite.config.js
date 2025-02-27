2df39ed59c4cbe8f4e34d149b4449da84fdd0452
Kurkuma <k.s.vasiuk@gmail.com>
2025-02-27 18:19:38 +0200

    updated git Action info

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command }) => {
   };
 
   if (command !== "serve") {
-    config.base = "/react-vite-project-template/";
+    config.base = "/FluChart/";
   }
 
   return config;
